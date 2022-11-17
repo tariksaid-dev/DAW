1e56349f4357e79d201ed262f3aff0efaad01309
tariksaiddev <tariksaid.dev@gmail.com>
2022-11-17 14:07:10 +0100

    New

--- PR/Tema3/src/test.java
+++ PR/Tema3/src/test.java
@@ -4,16 +4,6 @@ public class test {
 //         Ejercicio 28 : Haz un programa que pida 6 palabras al usuario y las guarde en un List<String>. El
 // programa deberá mostrar por pantalla las palabras únicas (o sea, las palabras repetidas no se
 // mostrarán).
-        System.out.println("Introduce 6 palabras separadas por un espacio:");
-        String ans = new Scanner(System.in).nextLine();
-        String[] words = ans.split(" ");
-        List<String> noRep = new ArrayList<>();
-        
-        for(String i : words) {
-            if(!noRep.contains(i)) {
-                noRep.add(i);
-            }
-        }
-        System.out.println(noRep.toString());
+
     }
 }
