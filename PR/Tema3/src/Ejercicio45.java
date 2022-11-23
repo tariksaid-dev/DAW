de33bd91ec4d54960f5698030ba2d7c8d4fed307
tariksaiddev <tariksaid.dev@gmail.com>
2022-11-23 02:43:18 +0100

    New

--- PR/Tema3/src/Ejercicio45.java
+++ PR/Tema3/src/Ejercicio45.java
@@ -1,19 +1,31 @@
+import java.util.Arrays;
+
+
 public class Ejercicio45 {
     public static void main(String[] args) {
         // data
         int[] a = {50, 26, 7, 9, 15, 27};
-        // loop for ordering the array
-        for(int i=0; i<a.length; i++) {
-            for(int j=0; j<a.length-1; j++) {
-                if(a[j] > a[j+1]) {
-                    int aux = a[j];
-                    a[j] = a[j+1];
-                    a[j+1] = aux;
+        // loop 1
+        for (int i = 0; i < a.length; i++) {
+            int temp = 0;
+            // variable para index
+            int min = i;
+            // loop 2, i+1 (no necesito comparar último elemento)
+            for (int j = i + 1; j < a.length; j++) {
+                // comparo elemento loop 1 con elemento loop 2
+                if (a[j] < a[min]) {
+                    // guardo posición del elemento más pequeño
+                    min = j; // first loop, min = a[2] == 7
                 }
             }
-        }
-        for(int i=0; i<a.length; i++) {
-            System.out.println(a[i]);
+            // para match con el proceso gráfico del enunciado
+            // System.out.println("i: " + i + " min: " + min);
+            if (i != 5) {
+                temp = a[i]; // temp = 50
+                a[i] = a[min]; // a[0] = 7
+                a[min] = temp; // a[2] = 50   
+                System.out.println(Arrays.toString(a));
+            }
         }
     }
 }
