8307f707bda7492a4801ce7b15843c1463061a02
tariksaiddev <tariksaid.dev@gmail.com>
2022-11-25 11:14:45 +0100

    New

--- PR/Tema3/src/test.java
+++ PR/Tema3/src/test.java
@@ -1,9 +1,7 @@
+import java.util.Arrays;
+
 public class test {
     public static void main(String[] args) {
-        
-        for(int i=0; i<5;i++) {
-            test2 a = new test2(i);
-            a.start();
-        }
+
     }
 }
