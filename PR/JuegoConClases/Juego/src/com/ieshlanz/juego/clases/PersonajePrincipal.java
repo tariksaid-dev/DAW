acab752c7f399a63978e6450e1be91bbbbfe82ec
tariksaid-dev <tariksaid.dev@gmail.com>
2022-12-22 10:17:16 +0100

    JUEGO CON CLASES

---
+++ PR/JuegoConClases/Juego/src/com/ieshlanz/juego/clases/PersonajePrincipal.java
@@ -0,0 +1,88 @@
+package com.ieshlanz.juego.clases;
+import bpc.daw.consola.*;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import javax.imageio.ImageIO;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
+public class PersonajePrincipal {
+    private static Rectangle [][] rectangulosPersonajePrincipal = {
+        {
+            new Rectangle (21, 11, 83, 116),
+            new Rectangle (123, 12, 95, 126),
+            new Rectangle (215, 2, 116, 118), 
+            new Rectangle (331, 10, 102, 123), 
+            new Rectangle (451, 13, 83, 110), 
+            new Rectangle (551, 12, 91, 123), 
+            new Rectangle (641, 10, 114, 129),
+            new Rectangle (776, 5, 98, 121)
+        }, 
+        {
+            new Rectangle (2, 148, 99, 115),
+            new Rectangle (101, 151, 114, 112),
+            new Rectangle (222, 152, 94, 121),
+            new Rectangle (325, 145, 93, 122),
+            new Rectangle (433, 152, 94, 105),
+            new Rectangle (540, 153, 109, 101),
+            new Rectangle (645, 159, 103, 131),
+            new Rectangle (763, 115, 82, 112)
+    }
+
+        
+    };
+        // Fila y columna del rectángulo del frame del personaje principal 
+        private int filaPersonajePrincipal = 0;
+        private int columnaPersonajePrincipal = 0;
+        private int lineaTiempoPersonajePrincipal = 0;
+        private Sprite sprite;
+
+        public PersonajePrincipal(CapaSprites sprites, Map<String, Image> fotos) {
+            this.filaPersonajePrincipal=0;
+            this.columnaPersonajePrincipal=0;
+            this.lineaTiempoPersonajePrincipal=0;
+            this.sprite = sprites.crearSprite(
+                fotos.get("animacion.png"),
+                rectangulosPersonajePrincipal[filaPersonajePrincipal][columnaPersonajePrincipal],
+                150, 
+                900
+            );
+        }
+
+        public void actuar(Graphics g, Teclado t) {
+            // movemos el muñeco con W A S D
+            if(t.teclaPulsada(KeyEvent.VK_W)) {
+                this.sprite.moverY(-4);
+            }
+            if(t.teclaPulsada(KeyEvent.VK_S)) {
+                this.sprite.moverY(4);
+            }
+            if(t.teclaPulsada(KeyEvent.VK_A)) {
+                this.sprite.moverX(-4);
+            }
+            if(t.teclaPulsada(KeyEvent.VK_D)) {
+                this.moverDerecha();
+            }
+        }
+
+        private void moverDerecha() {
+            this.sprite.moverX(4);
+            // Incrementamos la linea de tiempo del player
+            lineaTiempoPersonajePrincipal ++;
+            // Calculo con la linea de tiempo el frame que debo mostrar
+            columnaPersonajePrincipal = lineaTiempoPersonajePrincipal<10?0:
+                lineaTiempoPersonajePrincipal<20?1:
+                lineaTiempoPersonajePrincipal<30?2:
+                lineaTiempoPersonajePrincipal<40?3:
+                lineaTiempoPersonajePrincipal<50?4:
+                lineaTiempoPersonajePrincipal<60?5:
+                lineaTiempoPersonajePrincipal<70?6:
+                lineaTiempoPersonajePrincipal<80?7:0;
+            if(lineaTiempoPersonajePrincipal == 90) {
+               lineaTiempoPersonajePrincipal = 0;
+        }
+        this.sprite.setRectanguloFrame(rectangulosPersonajePrincipal[filaPersonajePrincipal][columnaPersonajePrincipal]);
+        }
+}
---
+++ PR/JuegoConClases/Juego/src/com/ieshlanz/juego/programa/App.java
@@ -0,0 +1,66 @@
+package com.ieshlanz.juego.programa;
+import com.ieshlanz.juego.clases.*;
+import java.awt.Graphics;
+import java.util.List;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import javax.imageio.ImageIO;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import bpc.daw.consola.*;
+
+public class App {
+    public static void main(String[] args)  {
+        Consola c = new Consola();
+        CapaTexto ct = c.getCapaTexto();
+        CapaCanvas canvas = c.getCapaCanvas();
+        CapaSprites sprites = c.getCapaSprites();
+        Graphics g = canvas.getGraphics();
+        Teclado t = c.getTeclado();
+
+        // Guardo la asociación nombre foto -> foto cargada
+        Map<String, Image> fotos = new HashMap<>();
+
+        // Cargamos las fotos
+        List<String> nombreFotos=List.of(
+            "animacion.png", "bat.png", "esqueleto.png", "fondo1.jpg", "fondo2.jpg",
+            "fondo3.jpg", "monstruo.png", "skull.png", "skull2.png"
+        );
+
+        int contador = 0;
+        for (String i: nombreFotos) {
+            ct.cls();
+            int porcentaje = (contador * 100) / nombreFotos.size();
+            ct.println("Cargando... " +i + " " + porcentaje + "%");
+            InputStream datos = ClassLoader.getSystemResourceAsStream("com/ieshlanz/juego/recursos/" + i);
+            try {
+                Image fotoCargada = ImageIO.read(datos);
+                fotos.put(i, fotoCargada);
+            } catch (IOException e) {
+                ct.println("Error al cargar el archivo " + i);
+                ct.println("Motivo" + e.getMessage());
+            }
+            contador ++;
+        }
+        ct.cls();
+
+
+        PersonajePrincipal player = new PersonajePrincipal(sprites, fotos);
+        
+   
+        // Game loop
+        boolean repetir = true;
+        while (repetir) {
+            if(t.teclaPulsada(KeyEvent.VK_ESCAPE)) {
+                repetir = false;
+            }
+            player.actuar(g, t);
+            // Con esto el while va a 60FPS
+            c.esperarSiguienteFrame();
+        }
+
+
+        // t.leerCaracter();
+    }
+}
