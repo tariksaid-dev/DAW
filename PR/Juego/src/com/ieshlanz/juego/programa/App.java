0e325b52c9a209f9431d8014bf3224365802fce6
tariksaid-dev <tariksaid.dev@gmail.com>
2022-12-22 09:41:39 +0100

    JUEGO PROGRAMACIÓN

--- PR/Juego/src/com/ieshlanz/juego/programa/App.java
+++ PR/Juego/src/com/ieshlanz/juego/programa/App.java
@@ -2,6 +2,8 @@ package com.ieshlanz.juego.programa;
 import java.awt.Graphics;
 import java.util.List;
 import java.awt.Image;
+import java.awt.event.KeyEvent;
+
 import javax.imageio.ImageIO;
 import java.util.*;
 import java.io.*;
@@ -44,10 +46,70 @@ public class App {
         ct.cls();
 
         // Cargamos los sprites
-        Rectangle r = new Rectangle(21, 11, 83, 116);
-        sprites.crearSprite(fotos.get("animacion.png"), r, 200, 200);
+        
+
+        Rectangle [][] rectangulosPersonajePrincipal = {
+            {
+                new Rectangle (21, 11, 83, 116),
+                new Rectangle (123, 12, 95, 126),
+                new Rectangle (215, 2, 116, 118), 
+                new Rectangle (331, 10, 102, 123), 
+                new Rectangle (451, 13, 83, 110), 
+                new Rectangle (551, 12, 91, 123), 
+                new Rectangle (641, 10, 114, 129),
+                new Rectangle (776, 5, 98, 121)
+            }, 
+            {
+                new Rectangle (2, 148, 99, 115),
+                new Rectangle (101, 151, 114, 112),
+                new Rectangle (222, 152, 94, 121),
+                new Rectangle (325, 145, 93, 122),
+                new Rectangle (433, 152, 94, 105),
+                new Rectangle (540, 153, 109, 101),
+                new Rectangle (645, 159, 103, 131),
+                new Rectangle (763, 115, 82, 112)
+            }
+        };
+
+        // Fila y columna del rectángulo del frame del personaje principal 
+        int filaPersonajePrincipal = 0;
+        int columnaPersonajePrincipal = 0;
+        
+        
+        Sprite player = sprites.crearSprite(
+            fotos.get("animacion.png"),
+            rectangulosPersonajePrincipal[filaPersonajePrincipal][columnaPersonajePrincipal],
+            150, 
+            900
+        );
+        
+        
+        boolean repetir = true; 
+        // Game loop
+        while (repetir) {
+            
+            if(t.teclaPulsada(KeyEvent.VK_ESCAPE)) {
+                repetir = false;
+            }
+            // movemos el muñeco con W A S D
+            if(t.teclaPulsada(KeyEvent.VK_W)) {
+                player.moverY(-4);
+            }
+            if(t.teclaPulsada(KeyEvent.VK_S)) {
+                player.moverY(4);
+            }
+            if(t.teclaPulsada(KeyEvent.VK_A)) {
+                player.moverX(-4);
+            }
+            if(t.teclaPulsada(KeyEvent.VK_D)) {
+                player.moverX(4);
+            }
+            
+            // Con esto el while va a 60FPS
+            c.esperarSiguienteFrame();
+        }
 
 
-        t.leerCaracter();
+        // t.leerCaracter();
     }
 }
