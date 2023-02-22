8d1c3b10d845e5cc06c7ef08baeaec27f46c66a9
tariksaid-dev <tariksaid.dev@gmail.com>
2023-02-22 01:40:41 +0100

    New

--- PR/Tema5/JuegoNaves_Framework/FrameworkJuego/src/hlanz/juego/consola/GameObject.java
+++
@@ -1,9 +0,0 @@
-package hlanz.juego.consola;
-
-import bpc.daw.consola.*;
-
-public abstract class GameObject {
-    protected Juego juego;
-    protected Consola consola;
-    protected Escena escena;
-}
