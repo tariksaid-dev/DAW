19e52a2d4227cc5c9a432f5d072a7d8c00689414
tariksaid-dev <tariksaid.dev@gmail.com>
2023-11-25 23:42:48 +0100

    Minor changes

--- DAW2/EC/T2/poke-api/src/hooks/useApi.js
+++ DAW2/EC/T2/poke-api/src/hooks/useApi.js
@@ -2,7 +2,7 @@ import { createRandomNumber } from "../../utils/createRandom";
 import { API_URL } from "../../constants/constants";
 
 export async function getPokemons() {
-  const pokemons = Array.from({ length: 9 }).map(async (element) => {
+  const pokemons = Array.from({ length: 9 }).map(async () => {
     const url = API_URL + createRandomNumber();
     const res = await fetch(url);
     const data = await res.json();
